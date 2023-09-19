bd84917bcb8f066d4ff72a22c2889b856e7f2b7b
Nikko Adrian <nikkoadrian02@gmail.com>
2023-09-19 11:49:02 +0700

    tampilan dashboard admin

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+<div class="float-right d-none d-sm-block">
+    (<b>Application</b> v: 1.2.0)
+    (<b>Laravel</b> v: {{ Illuminate\Foundation\Application::VERSION }})
+    (<b>PHP</b> v: {{ PHP_VERSION }})
+</div>
+<strong>Copyright &copy; 2020-2023 <a href="https://github.com/Nikkoadr">Nikko Adrian</a>.</strong> All rights reserved.
+</footer>
---
+++ resources/views/layout/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<!-- Left navbar links -->
+<ul class="navbar-nav">
+    <li class="nav-item">
+    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+    <a href="/" class="nav-link">Website</a>
+    </li>
+</ul>
+
+<!-- Right navbar links -->
+<ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+            <i class="fa-solid fa-right-from-bracket"></i>
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </li>
+</ul>
+</nav>
+<!-- /.navbar -->
